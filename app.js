abcc096285c5283368ae54ac31dfbc34b4b5052e
philschatz <253202+philschatz@users.noreply.github.com>
2021-06-21 22:18:07 -0500

    fetch book list from ABL and select books

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', () => {
+    const ablUrl = '/content-manager-approved-books/approved-book-list.json'
     const serverRootUrl = 'https://archive-staging.cnx.org/contents'
     const legacyRoot = 'https://legacy.cnx.org/content'
     const namespacePrefixes = {
@@ -10,57 +11,6 @@ window.addEventListener('load', () => {
         'mml'   : 'http://www.w3.org/1998/Math/MathML',
         'mathml': 'http://www.w3.org/1998/Math/MathML'
     }
-    const bookUuids = [
-        /* Algebra & Trigonometry */ '13ac107a-f15f-49d2-97e8-60ab2e3b519c',
-        /* Am Gov 2e */ '9d8df601-4f12-4ac1-8224-b450bf739e5f',
-        /* Anatomy & Physiology */ '14fb4ad7-39a1-4eee-ab6e-3ef2482e3e22',
-        /* Astronomy */ '2e737be8-ea65-48c3-aa0a-9f35b4c6a966',
-        /* Biology for AP Courses */ '6c322e32-9fb0-4c4d-a1d7-20c95c5c7af2',
-        /* Biology 2e */ '8d50a0af-948b-4204-a71d-4826cba765b8',
-        /* Business Ethics */ '914ac66e-e1ec-486d-8a9c-97b0f7a99774',
-        /* Business statistics */ 'b56bb9e9-5eb8-48ef-9939-88b1b12ce22f',
-        /* Business in Law I */ '464a3fba-68c1-426a-99f9-597e739dc911',
-        /* Calculus vol 1 */ '8b89d172-2927-466f-8661-01abc7ccdba4',
-        /* Calculus vol 2 */ '1d39a348-071f-4537-85b6-c98912458c3c',
-        /* Calculus vol 3 */ 'a31cd793-2162-4e9e-acb5-6e6bbd76a5fa',
-        /* Chemistry 2e */ '7fccc9cf-9b71-44f6-800b-f9457fd64335',
-        /* Chemistry: Atoms First 2e */ 'd9b85ee6-c57f-4861-8208-5ddf261e9c5f',
-        /* College Algebra */ '9b08c294-057f-4201-9f48-5d6ad992740d',
-        /* College Physics */ '031da8d3-b525-429c-80cf-6c8ed997733a',
-        /* College Physics for AP */ '8d04a686-d5e8-4798-a27d-c608e4d0e187',
-        /* College Success */ 'e8668a14-9a7d-4d74-b58c-3681f8351224',
-        /* Concepts of Biology */ 'b3c1e1d2-839c-42b0-a314-e119a8aafbdd',
-        /* Elementary Algebra 2e */ '55931856-c627-418b-a56f-1dd0007683a8',
-        /* Entrepreneurship */ 'd380510e-6145-4625-b19a-4fa68204b6b1',
-        /* Intermediate Algebra 2e */ '4664c267-cd62-4a99-8b28-1cb9b3aee347',
-        /* Introduction to Business */ '4e09771f-a8aa-40ce-9063-aa58cc24e77f',
-        /* Introduction to Sociology 2e */ '02040312-72c8-441e-a685-20e9333f3e1d',
-        /* Introductory Statistics */ '30189442-6998-4686-ac05-ed152b91b9de',
-        /* Microbiology */ 'e42bd376-624b-4c0f-972f-e0c57998e765',
-        /* Organizational Behavior */ '2d941ab9-ac5b-4eb8-b21c-965d36a4f296',
-        /* Psychology 1e */ '4abf04bf-93a0-45c3-9cbc-2cefd46e68cc',
-        /* Psychology 2e */ '06aba565-9432-40f6-97ee-b8a361f118a8',
-        /* Prealgebra 2e */ 'f0fa90be-fca8-43c9-9aad-715c0a2cee2b',
-        /* Precalculus */ 'fd53eae1-fa23-47c7-bb1b-972349835c3c',
-        /* Principles of Accounting Vol 1 */ '9ab4ba6d-1e48-486d-a2de-38ae1617ca84',
-        /* Principles of Accounting Vol 2 */ '920d1c8a-606c-4888-bfd4-d1ee27ce1795',
-        /* Principles of MicroEcon for AP courses 2e */ '636cbfd9-4e37-4575-83ab-9dec9029ca4e',
-        /* Principles of MacroEcon for AP courses 2e */ '9117cf8c-a8a3-4875-8361-9cb0f1fc9362',
-        /* Principles of Economics 2e */ 'bc498e1f-efe9-43a0-8dea-d3569ad09a82',
-        /* Principles of Macroeconomics 2e */ '27f59064-990e-48f1-b604-5188b9086c29',
-        /* Principles of Management */ 'c3acb2ab-7d5c-45ad-b3cd-e59673fedd4e',
-        /* Principles of Microeconomics 2e */ '5c09762c-b540-47d3-9541-dda1f44f16e5',
-        /* Statistics (High School) */ '394a1101-fd8f-4875-84fa-55f15b06ba66',
-        /* US History */ 'a7ba2fb8-8925-4987-b182-5f4429d48daa',
-        /* University Physics vol 1 */ 'd50f6e32-0fda-46ef-a362-9bd36ca7c97d',
-        /* University Physics vol 2 */ '7a0f9770-1c44-4acd-9920-1cd9a99f2a1e',
-        /* University Physics vol 3 */ 'af275420-6050-4707-995c-57b9cc13c358',
-
-        /* Polish Physics 1 */ '4eaa8f03-88a8-485a-a777-dd3602f6c13e',
-        /* Polish Physics 2 */ '16ab5b96-4598-45f9-993c-b8d78d82b0c6',
-        /* Polish Physics 3 */ 'bb62933e-f20a-4ffc-90aa-97b36c296c3e',
-
-    ]
 
     const qs = (sel) => {
         const el = document.querySelector(sel)
@@ -78,14 +28,14 @@ window.addEventListener('load', () => {
     const bookCountEl = qs('#book-count')
     const analyzeCodeEl = qs('#analyze-code')
     const stopAfterNPages = qs('#stop-after-n-pages')
+    const booksEl = qs('#books')
+    const validationMessageEl = qs('#validation-message')
     const form = qs('form')
-    const sourceFormat = form.elements['sourceFormat']
-
-    bookCountEl.textContent = bookUuids.length
-    
+    const sourceFormat = form.elements['sourceFormat']    
 
     let isStopping = false
     let isSkipping = false
+    bookCountEl.setAttribute('href', ablUrl)
 
     const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
     function* times(x) {
@@ -114,9 +64,15 @@ window.addEventListener('load', () => {
     const fetchText = async (url) => (await fetch(url)).text() 
 
     let isValid = false
+    function getSelectedBookUUIDs() {
+        return Array.from(booksEl.querySelectorAll("option:checked"), e => e.value)
+    }
     const getValidationMessage = () => {
         if (sourceFormat.value !== 'xhtml' && sourceFormat.value !== 'cnxml') {
             return 'Choose a format to search: XHTML or CNXML'
+        }
+        if (getSelectedBookUUIDs().length === 0) {
+            return 'Select at least one book'
         }
         const selector = selectorEl.value
         if (selector[0] === '/') {
@@ -142,11 +98,20 @@ window.addEventListener('load', () => {
         const msg = getValidationMessage()
         selectorEl.setCustomValidity(msg)
         isValid = !msg
+        validationMessageEl.textContent = msg
     }
     selectorEl.addEventListener('input', validateSelector)
     selectorEl.addEventListener('keyup', validateSelector)
     form.addEventListener('change', validateSelector)
 
+    startEl.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        const params = new URLSearchParams(new FormData(form))
+        params.delete('books')
+        window.location.hash = params.toString()
+        doStart()
+    })
     skipEl.addEventListener('click', (e) => { e.preventDefault(); isSkipping = true } )
     stopEl.addEventListener('click', (e) => { e.preventDefault(); isStopping = true } )
     const startFn = async () => {
@@ -160,12 +125,15 @@ window.addEventListener('load', () => {
         const selector = selectorEl.value
         const analyzeFn = new Function('type', 'json', 'bookUuid', 'pageUuid', analyzeCodeEl.value)
 
+        const bookUuids = getSelectedBookUUIDs()
+
         // clear
         isStopping = false
         resultsEl.innerHTML = ''
 
         selectorEl.disabled = true
         startEl.disabled = true
+        booksEl.disabled = true
         stopEl.disabled = false
         skipEl.disabled = false
 
@@ -253,6 +221,7 @@ window.addEventListener('load', () => {
                 if (isStopping) {
                     selectorEl.disabled = false
                     startEl.disabled = false
+                    booksEl.disabled = false
                     stopEl.disabled = true
                     skipEl.disabled = true
                     return
@@ -267,22 +236,16 @@ window.addEventListener('load', () => {
 
         selectorEl.disabled = false
         startEl.disabled = false
+        booksEl.disabled = false
         stopEl.disabled = true
         skipEl.disabled = true
 
     }
 
     // load the form from the URL
-    if (window.location.hash.length > 1) {
-        const state = JSON.parse(decodeURIComponent(window.location.hash.substring(1)))
-        selectorEl.value = state.q
-        if (state.code) {
-            analyzeCodeEl.value = state.code
-        }
-        validateSelector()
-    }
-    if (window.location.search.length > 1) {
-        const args = querystring(window.location.search.substring(1))
+    const querystringOrHash = window.location.search || window.location.hash
+    if (querystringOrHash.length > 1) {
+        const args = querystring(querystringOrHash.substring(1))
         for (const el of form.elements) {
             const key = el.name
             if (args[key]) {
@@ -293,17 +256,53 @@ window.addEventListener('load', () => {
     }
 
     // Run!
-    if (isValid) {
-        startFn().then(null, (err) => {
-            console.error(err.message)
-            alert(`Error: ${err.message}`)
-            selectorEl.disabled = false
-            startEl.disabled = false
-            stopEl.disabled = true
-            skipEl.disabled = true
-        })
+    function doStart() {
+        if (isValid) {
+            startFn().then(null, (err) => {
+                console.error(err.message)
+                alert(`Error: ${err.message}`)
+                selectorEl.disabled = false
+                startEl.disabled = false
+                stopEl.disabled = true
+                skipEl.disabled = true
+            })
+        }
     }
 
+    fetch(ablUrl).then(async res => {
+        const approvedBookList = await res.json()
+        console.log('Here is the approved book list:', approvedBookList)
+        const bookIdAndSlugs = []
+
+        // collect all the books & remember if they are archive/git
+        approvedBookList.approved_books.forEach(bookContainer => {
+            bookContainer.books.forEach(book => bookIdAndSlugs.push({...bookContainer, ...book}))
+        })
+        // sort by Book slug
+        bookIdAndSlugs.sort((a, b) => {
+            if (a.slug.toLowerCase() < b.slug.toLowerCase())
+                return -1
+            if (a.slug.toLowerCase() > b.slug.toLowerCase())
+                return 1
+            return 0
+        })
+        bookIdAndSlugs.forEach(({uuid, slug, collection_id, server}) => {
+            const o = document.createElement('option')
+            o.setAttribute('value', uuid)
+            // Git books are not supported yet
+            if (collection_id) {
+                o.setAttribute('selected', 'selected')
+            } else {
+                o.setAttribute('disabled', 'disabled')
+            }
+            o.append(slug)
+            booksEl.append(o)
+        })
+
+        bookCountEl.textContent = bookIdAndSlugs.length
+        validateSelector()
+
+    }, err => alert(`Problem getting the approved book list. Maybe you are offline or the URL has moved. '${ablUrl}'`))
 
     function recLeafPages(acc, node) {
         if (node.contents) {
---
+++ content-manager-approved-books/README.md
@@ -0,0 +1,3 @@
+This is a fake file for testing purposes only.
+
+On github.io, this is provided by [this repo](https://github.com/openstax/content-manager-approved-books).
